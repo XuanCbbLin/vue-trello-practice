32cff0552966a2459560a5dcff1b4244531ab916
Xuan <xuancbblin@gmail.com>
2023-01-18 16:12:52 +0800

    BoardView change template and script position

--- src/views/BoardView.vue
+++ src/views/BoardView.vue
@@ -1,3 +1,20 @@
+<template>
+  <div class="bg-emerald-700 h-[100vh] w-full block overflow-x-auto overflow-y-hidden">
+    <div id="board-wrapper" class="h-full w-full p-4 block overflow-auto">
+      <div class="flex flex-row items-start">
+        <!-- card -->
+        <CardWrapper v-for="card in list" :key="card.id" v-bind="card" />
+
+        <!-- add new card -->
+        <AddNewCard />
+      </div>
+    </div>
+
+    <!-- lightbox -->
+    <EditBox v-if="currentEditTask?.id" />
+  </div>
+</template>
+
 <script setup lang="ts">
 import { computed } from 'vue';
 import { useStore } from '@/stores';
@@ -20,20 +37,3 @@ const store = useStore();
 const list = computed<Lists[]>(() => store.lists);
 const currentEditTask = computed(() => store.currentEditTask);
 </script>
-
-<template>
-  <div class="bg-emerald-700 h-[100vh] w-full block overflow-x-auto overflow-y-hidden">
-    <div id="board-wrapper" class="h-full w-full p-4 block overflow-auto">
-      <div class="flex flex-row items-start">
-        <!-- card -->
-        <CardWrapper v-for="card in list" :key="card.id" v-bind="card" />
-
-        <!-- add new card -->
-        <AddNewCard />
-      </div>
-    </div>
-
-    <!-- lightbox -->
-    <EditBox v-if="currentEditTask?.id" />
-  </div>
-</template>
