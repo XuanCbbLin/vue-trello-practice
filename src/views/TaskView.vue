a6485152b533c262e7a54b503f328af574de724c
xuan <xuancbblin@gmail.com>
2023-01-20 16:06:13 +0800

    feat : finish enter route open Lightbox

--- src/views/TaskView.vue
+++ src/views/TaskView.vue
@@ -3,11 +3,23 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, onMounted } from 'vue';
 import { useStore } from '@/stores';
+import { useRoute } from 'vue-router';
 
 const store = useStore();
 const currentEditTask = computed(() => store.currentEditTask);
+const { openEditTask } = store;
+const route = useRoute();
+const { params } = route;
+const cardId = computed(() => params.cardId as string);
+const taskId = computed(() => params.taskId as string);
+
+onMounted(() => {
+  if (cardId.value && taskId.value) {
+    openEditTask(cardId.value, taskId.value);
+  }
+});
 </script>
 
 <style scoped></style>
